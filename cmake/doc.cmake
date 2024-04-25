390e6e8cab1597a71a4a334897c1debd8f7be7af
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2024-04-25 12:49:20 +0200

    cmake/doc: Generate files in binary directory
    Generating outputs in the build directory is preferred over doing so in the
    source directory. We also need to be consistent in using CMAKE_CURRENT_XXX_DIR
    or CMAKE_XXX_DIR, in case someone ever embeds this cmake project in another
    one.
    
    Signed-off-by: Jeremi Piotrowski <jpiotrowski@microsoft.com>

--- cmake/doc.cmake
+++ cmake/doc.cmake
@@ -7,22 +7,22 @@ endif()
 string(TIMESTAMP TODAY "%B %d, %Y")
 
 add_custom_command(
-	OUTPUT ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.1
+	OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1
 	COMMAND ${PANDOC_EXECUTABLE}
 		${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.md
 		--standalone --from markdown --to man
 		--variable footer="azure-nvme-id ${VERSION}"
 		--variable date="${TODAY}"
-		-o ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.1
+		-o ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1
 	DEPENDS ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.md
 	COMMENT "Generating manpage for azure-nvme-id"
 )
 
 add_custom_target(
         doc ALL
-        DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/doc/azure-nvme-id.1
+        DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1
 )
 
 set(MANPAGES_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/share/man")
-install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/doc/azure-nvme-id.1
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1
         DESTINATION ${MANPAGES_INSTALL_DIR}/man1)
