d2c0dd7a10fbfbc4caa851cdb91dc1a8b0bf3a63
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2024-04-25 12:46:25 +0200

    cmake/doc: Fix install failure if pandoc is not found
    If pandoc is not found, make install fails due to missing doc/azure-nvme-id.1.
    Reorganize the cmake code to return early when pandoc is not found and skip the
    install/dependency definitions in that case.
    
    Signed-off-by: Jeremi Piotrowski <jpiotrowski@microsoft.com>

--- cmake/doc.cmake
+++ cmake/doc.cmake
@@ -1,22 +1,22 @@
 find_program(PANDOC_EXECUTABLE pandoc)
+if(NOT PANDOC_EXECUTABLE)
+        message(WARNING "Pandoc not found, will not generate manpages")
+        return()
+endif()
 
 string(TIMESTAMP TODAY "%B %d, %Y")
 
-if(PANDOC_EXECUTABLE)
-        add_custom_command(
-                OUTPUT ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.1
-                COMMAND ${PANDOC_EXECUTABLE}
-                        ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.md
-                        --standalone --from markdown --to man
-                        --variable footer="azure-nvme-id ${VERSION}"
-                        --variable date="${TODAY}"
-                        -o ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.1
-                DEPENDS ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.md
-                COMMENT "Generating manpage for azure-nvme-id"
-        )
-else()
-        message(WARNING "Pandoc not found, will not generate manpages")
-endif()
+add_custom_command(
+	OUTPUT ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.1
+	COMMAND ${PANDOC_EXECUTABLE}
+		${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.md
+		--standalone --from markdown --to man
+		--variable footer="azure-nvme-id ${VERSION}"
+		--variable date="${TODAY}"
+		-o ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.1
+	DEPENDS ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.md
+	COMMENT "Generating manpage for azure-nvme-id"
+)
 
 add_custom_target(
         doc ALL
