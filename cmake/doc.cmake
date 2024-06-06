2c9651e42c777201766e8935107831b10b185ee3
Chris Patterson <cpatterson@microsoft.com>
2024-06-06 09:08:02 -0400

    build(doc): generate manpages only with -DGENERATE_MANPAGES=1
    As pointed out by Noah in #15, Debian packaging lints issues in the
    generated manpage due to formatting emitted by older versions of pandoc:
    https://github.com/jgm/pandoc/issues/9020
    
    While changes in recent versions of pandoc have improved the output,
    let's just take the generated output from pandoc 3.2 as a starting point
    and maintain it manually for the time being.  This reduces friction for
    packaging and gives us flexibility if we wanted to do something in the
    manpage that pandoc doesn't support.
    
    Allow manpages to be generated at build-time with -DGENERATE_MANPAGES=1
    which will make it easy to sync.
    
    Signed-off-by: Chris Patterson <cpatterson@microsoft.com>

--- cmake/doc.cmake
+++ cmake/doc.cmake
@@ -1,23 +1,34 @@
-find_program(PANDOC_EXECUTABLE pandoc)
-if(NOT PANDOC_EXECUTABLE)
-        message(WARNING "Pandoc not found, will not generate manpages")
-        return()
-endif()
-
 string(TIMESTAMP TODAY "%B %d, %Y")
 
-add_custom_command(
-	OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1
-	COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/doc
-	COMMAND ${PANDOC_EXECUTABLE}
-		${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.md
-		--standalone --from markdown --to man
-		--variable footer="azure-nvme-id ${VERSION}"
-		--variable date="${TODAY}"
-		-o ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1
-	DEPENDS ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.md
-	COMMENT "Generating manpage for azure-nvme-id"
-)
+option(GENERATE_MANPAGES "Generate manpages with pandoc" OFF)
+if(GENERATE_MANPAGES)
+    find_program(PANDOC_EXECUTABLE pandoc)
+    if(NOT PANDOC_EXECUTABLE)
+        message(WARNING "Pandoc not found, will not generate manpages")
+        return()
+    endif()
+
+    add_custom_command(
+        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/doc
+        COMMAND ${PANDOC_EXECUTABLE}
+            ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.md
+            --standalone --from markdown --to man
+            --variable footer="azure-nvme-id ${VERSION}"
+            --variable date="${TODAY}"
+            -o ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1
+        DEPENDS ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.md
+        COMMENT "Generating manpage for azure-nvme-id"
+    )
+else()
+    configure_file(
+        "${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.1"
+        "${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1"
+        @ONLY
+    )
+endif()
+
+
 
 add_custom_target(
         doc ALL
