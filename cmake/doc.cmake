6737876daf66d009af8411ca8efcb72ffc3bcee2
Chris Patterson <cpatterson@microsoft.com>
2024-06-14 16:01:14 -0400

    feat(doc): rename azure-nvme-id.1 -> azure-nvme-id.8
    Section 8 is meant System administration commands and daemons, which
    azure-nvme-id falls under.
    
    Signed-off-by: Chris Patterson <cpatterson@microsoft.com>

--- cmake/doc.cmake
+++ cmake/doc.cmake
@@ -9,21 +9,21 @@ if(GENERATE_MANPAGES)
     endif()
 
     add_custom_command(
-        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1
+        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.8
         COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/doc
         COMMAND ${PANDOC_EXECUTABLE}
             ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.md
             --standalone --from markdown --to man
             --variable footer="azure-nvme-id ${VERSION}"
             --variable date="${TODAY}"
-            -o ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1
+            -o ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.8
         DEPENDS ${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.md
         COMMENT "Generating manpage for azure-nvme-id"
     )
 else()
     configure_file(
-        "${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.1"
-        "${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1"
+        "${CMAKE_SOURCE_DIR}/doc/azure-nvme-id.8"
+        "${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.8"
         @ONLY
     )
 endif()
@@ -32,9 +32,9 @@ endif()
 
 add_custom_target(
         doc ALL
-        DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1
+        DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.8
 )
 
 set(MANPAGES_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/share/man")
-install(FILES ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.1
-        DESTINATION ${MANPAGES_INSTALL_DIR}/man1)
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/doc/azure-nvme-id.8
+        DESTINATION ${MANPAGES_INSTALL_DIR}/man8)
